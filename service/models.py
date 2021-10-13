004d457ef7756f2d6408703f1350081f2499f6cd
aubrey <aaaaubreys@gmail.com>
2021-10-13 11:06:15 -0400

    add product model

--- service/models.py
+++ service/models.py
@@ -13,7 +13,6 @@ db = SQLAlchemy()
 
 class DataValidationError(Exception):
     """ Used for an data validation errors when deserializing """
-
     pass
 
 ######################################################################
