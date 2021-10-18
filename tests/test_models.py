ba3b5617626f8c0c9133953186deec17c818c788
itz-JR <jaroberts12@gmail.com>
2021-10-18 11:34:41 -0400

    create shopcart methods and tests with some required bug fixes

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,40 +1,55 @@
 """
-Test cases for YourResourceModel Model
+Test cases for Shopcart Model
 
 """
 import logging
 import unittest
 import os
-from service.models import YourResourceModel, DataValidationError, db
+from service.models import Shopcart, Product, DataValidationError, db
+from service import app
+
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgres://postgres:postgres@localhost:5432/testdb"
+)
 
 ######################################################################
-#  <your resource name>   M O D E L   T E S T   C A S E S
+#  SHOPCART   M O D E L   T E S T   C A S E S
 ######################################################################
-class TestYourResourceModel(unittest.TestCase):
-    """ Test Cases for YourResourceModel Model """
+class TestShopcartModel(unittest.TestCase):
+    """ Test Cases for Shopcart Model """
 
     @classmethod
     def setUpClass(cls):
-        """ This runs once before the entire test suite """
-        pass
+        """This runs once before the entire test suite"""
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        Shopcart.init_db(app)
 
     @classmethod
     def tearDownClass(cls):
-        """ This runs once after the entire test suite """
-        pass
+        db.session.close()
 
     def setUp(self):
-        """ This runs before each test """
-        pass
+        db.drop_all()  # clean up the last tests
+        db.create_all()  # make our sqlalchemy tables
 
     def tearDown(self):
         """ This runs after each test """
-        pass
+        db.session.remove()
+        db.drop_all()
 
     ######################################################################
     #  T E S T   C A S E S
     ######################################################################
 
-    def test_XXXX(self):
-        """ Test something """
-        self.assertTrue(True)
+    def test_create_a_shopcart(self):
+        """Create a Shopcart and assert that it exists"""
+        shopcart = Shopcart(customer_id=123456)
+        self.assertTrue(shopcart != None)
+        self.assertEqual(shopcart.customer_id, 123456)
+
+        shopcart = Shopcart()
+        self.assertEqual(shopcart.customer_id, None)
+
